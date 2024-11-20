f8025021a759bc47f2ab158d59ea4a4c09f4c935
ALi ASNAWi <ali_asnawi93@yahoo.com>
2024-11-20 11:29:26 +0800

    minimum style front page

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -1,6 +1,6 @@
 import { Button, Navbar } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
-
+import '../style.css';
 
 function FrontPage() {
     const navigate = useNavigate();
@@ -18,15 +18,25 @@ function FrontPage() {
                 </Navbar.Collapse>
             </Navbar>
             <div className="container" style={{ padding: "30px", display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <div className='row'>
-                    <div className="col" style={{ textAlign: 'center' }}>{`"Fail to plan, plan to fail"`}</div>
-                    <div className="col" style={{ textAlign: 'center' }}>{`"Forgotful? Make a reminder"`}</div>
-                    <div className="col" style={{ textAlign: 'center' }}>{`"Simplified your thoughts, make life simpler"`}</div>
+                <div className='row' style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                    <div className="col" style={{ textAlign: 'center', fontFamily: 'monospace' }}>
+                        {`"Fail to plan, plan to fail"`}
+                    </div>
+                    <div className='vertical-line' style={{ height: '50px', width: '0.5px', backgroundColor: "#e0af79", margin: '0 20px', transform: 'scaleX(0.2)' }}></div>
+                    <div className="col" style={{ textAlign: 'center', fontFamily: 'monospace' }}>
+                        {`"Forgotful? Make a reminder"`}
+                    </div>
+                    <div className='vertical-line' style={{ height: '50px', width: '0.5px', backgroundColor: "#e0af79", margin: '0 20px', transform: 'scaleX(0.2)' }}></div>
+                    <div className="col" style={{ textAlign: 'center', fontFamily: 'monospace' }}>
+                        {`"Simplified your thoughts, make life simpler"`}
+                    </div>
                 </div>
             </div>
             <hr style={{ border: '1px solid black', margin: '20px 0' }} />
-            <div style={{ padding: "20px", textAlign: 'center' }}>
-                <h2>Features</h2>
+            <div style={{ padding: "20px", textAlign: 'center', fontFamily: 'Roboto', fontSize: '40px', color: '#333333', overflow: 'hidden', width: '100%' }}>
+                <h2 style={{ display: 'inline-block', whiteSpace: 'nowrap', animation: 'moveRight 5s linear infinite' }}>
+                    Features
+                </h2>
             </div>
 
             <hr style={{ border: '1px solid black', margin: '20px 0' }} />
@@ -46,35 +56,35 @@ function FrontPage() {
                         <img src='/images/no reminder.png' className='d-block w-100' alt='no reminders' />
                         <div className="carousel-caption d-none d-md-block">
                             <h5>Profile page</h5>
-                            <p>Create, edit, and delete reminders</p>
+                            <p>Create reminders, Add photos, and check weather</p>
                         </div>
                     </div>
                     <div className='carousel-item'>
                         <img src='/images/create a reminder.png' className='d-block w-100' alt='create a reminder' />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>Profile page</h5>
-                            <p>Create, edit, and delete reminders</p>
+                            <h5>Create a reminder page</h5>
+                            <p>Write down anything you want to remember</p>
                         </div>
                     </div>
                     <div className='carousel-item'>
                         <img src='/images/show in profile page.png' className='d-block w-100' alt='show in profile' />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>Profile page</h5>
-                            <p>Create, edit, and delete reminders</p>
+                            <h5>Updated Reminder</h5>
+                            <p>User can change status, edit content and delete reminder</p>
                         </div>
                     </div>
                     <div className='carousel-item'>
                         <img src='/images/check weather.png' className='d-block w-100' alt='check weather' />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>Profile page</h5>
-                            <p>Create, edit, and delete reminders</p>
+                            <h5>Side Features</h5>
+                            <p>Check current weather any place in this world with a single click</p>
                         </div>
                     </div>
                     <div className='carousel-item'>
                         <img src='/images/gallery.png' className='d-block w-100' alt='gallery' />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>Profile page</h5>
-                            <p>Create, edit, and delete reminders</p>
+                            <h5>Gallery Section</h5>
+                            <p>Add photo, view and delete</p>
                         </div>
                     </div>
                 </div>
